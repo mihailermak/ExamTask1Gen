7a9202a8d1834c6c65cf61b9d1e61ce6cf92a694
mihailermak <mihailermak1@yandex.ru>
2023-12-25 21:07:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def generation(size):
     array = [[0 for x in range(size)] for y in range(size)]
@@ -11,14 +13,28 @@ def generation(size):
 def checkmatrix(array):
     for row in array:
         count = sum(row)
-        if not (count >= 2 and count <= 5):
+        if not (count >= 2 and count <= 4):
             return False
     return True
+def savegraph(resultmatrix):
+    G = nx.Graph()
+
+    for i in range(1, len(resultmatrix)):
+        G.add_node(i)
+
+    for i in range(len(resultmatrix)):
+        for j in range(i + 1, len(resultmatrix)):
+            if resultmatrix[i][j] == 1:
+                G.add_edge(i, j)
+
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, font_weight='bold', font_size=12)
+    plt.savefig('graph.png')
 
 size = 8
 resultmatrix = generation(size)
 while not checkmatrix(resultmatrix):
     resultmatrix = generation(size)
 
-for row in resultmatrix:
-    print(row)
+print(resultmatrix)
+print(savegraph(resultmatrix))
